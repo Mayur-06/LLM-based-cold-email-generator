c4377eff6b7da002f236ad6d6bc25f94d4742c58
Mayur-06 <118823310+Mayur-06@users.noreply.github.com>
2025-01-22 17:56:38 +0530

    Update chains.py

--- chains.py
+++ chains.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("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"), model_name="llama-3.1-70b-versatile")
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
@@ -57,4 +57,4 @@ class Chain:
         return res.content
 
 if __name__ == "__main__":
-    print(os.getenv("GROQ_API_KEY"))
+    print(os.getenv("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"))
